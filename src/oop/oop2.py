186aff6d65f745ffce505be2218245485a112d71
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-05-31 08:48:27 -0700

    class using super inheritence and changing default values, the prining vehicles list

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -1,24 +1,23 @@
-# To the GroundVehicle class, add method drive() that returns "vroooom".
-#
-# Also change it so the num_wheels defaults to 4 if not specified when the
-# object is constructed.
-
+# To the GroundVehicle class,
 class GroundVehicle():
-    def __init__(self, num_wheels):
+#Change it so the num_wheels defaults to 4 if not specified when the
+# object is constructed.    
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
-
+#add method drive() that returns "vroooom".        
+    def drive(self):
+        return "vroooom"
     # TODO
-
-
 # Subclass Motorcycle from GroundVehicle.
-#
+class Motorcycle(GroundVehicle):
 # Make it so when you instantiate a Motorcycle, it automatically sets the number
-# of wheels to 2 by passing that to the constructor of its superclass.
-#
+# of wheels to 2 by passing that to the constructor of its superclass.    
+    def __init__(self, num_wheels = 2):
+        self.num_wheels = num_wheels    
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
-
+    def drive(self):
+        return "BRAAAP!!"
 # TODO
-
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
@@ -26,7 +25,7 @@ vehicles = [
     GroundVehicle(),
     Motorcycle(),
 ]
-
 # Go through the vehicles list and print the result of calling drive() on each.
-
+for vehicle in vehicles:
+    print(vehicle.drive())
 # TODO
